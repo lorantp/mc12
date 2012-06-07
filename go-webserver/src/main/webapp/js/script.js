d868ef62592b397dee95548ab5de1b471956de9b
Bart Enkelaar <b.enkelaar@topdesk.com>
2012-06-07 16:55:49 +0200

    MCXII-9 - Initial HTML page with "board"

--- go-webserver/src/main/webapp/js/script.js
+++ go-webserver/src/main/webapp/js/script.js
@@ -1,8 +1,40 @@
 /* Author:
 
 */
+var drawBoard = function() {
+	var board = document.getElementById("board");
+	var boardSize = 19;
+	for (var y = 0; y < boardSize; y++) {
+		for (var x = 0; x < boardSize; x++) {
+			board.appendChild(createSquare(x, y));
+		}
+	}
+}
 
+var createSquare = function(x, y) {
+	var div = document.createElement("div");
+	div.className = "position";
+	if (x % 2 === 0) {
+		div.setAttribute("stone", "black");
+	} else {
+		div.setAttribute("stone", "white");
+	}
+	div.setAttribute("column", x);
+	div.setAttribute("row", y);
+	
+	div.setAttribute("onClick", "createShow(" + x + ", " + y + ")");
 
+	div.setAttribute("style", createLocationStyle(x, y));
+	
+	return div;
+}
 
+var createLocationStyle = function(x, y) {
+	var top = y * 48;
+	var left = x * 48;
+	return "top: " + top + "px; left: " + left + "px;";
+}
 
-
+var createShow = function(x, y) {
+	alert("x: " + x + " y: " + y);
+}
