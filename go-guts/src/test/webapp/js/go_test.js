3add040dc02c64861ed1725dc081b2ca3084112d
Bart Enkelaar <b.enkelaar@topdesk.com>
2012-06-11 11:58:00 +0200

    Refactored go.js to fully use jQuery objects

--- go-guts/src/test/webapp/js/go_test.js
+++ go-guts/src/test/webapp/js/go_test.js
@@ -1,12 +1,12 @@
 describe("location style format", function() {
   it("should be of the format", function() {
-	  expect(createLocationStyle(0,0)).toBe("top: 0px; left: 0px;");
+	  expect(createLocationStyle(0, 0)).toEqual({top: "0px", left: "0px"});
   })
 });
 
 describe("position square attributes", function() {
 	it("should have correct attributes", function() {
-		var div = createSquare(0, 0);
+		var div = createSquare(0, 0).get(0);
 		expect(div.className).toBe("position");
 		expect(div.id).toBe("x0y0");
 		expect(div.style.top).toBe('0px');
