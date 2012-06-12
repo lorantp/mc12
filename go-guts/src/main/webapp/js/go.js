bdf4a57820f2d646be4c31a35f213da8f39e0506
Bart Enkelaar <b.enkelaar@topdesk.com>
2012-06-12 15:18:52 +0200

    (Re)Extracted go.js

---
+++ go-guts/src/main/webapp/js/go.js
@@ -0,0 +1,13 @@
+var draw = function() {
+	var rest = REST();
+	rest.getGame(1, function(data) {
+  		if (data) {
+	  		var actions = ACTIONS(rest, data.board.id, data.board.moves.length);
+  			var board = BOARD(data.board, actions);
+  			
+  			board.drawBoard();
+  			board.activateButton();
+			METADATA($).showData(data);
+		}
+	});
+};
