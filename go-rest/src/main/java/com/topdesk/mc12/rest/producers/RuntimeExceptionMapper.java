56989c522f1c4efae0897ca5f930d0db067a028d
Jorn Hertsig <jornh@topdesk.com>
2012-06-15 13:51:49 +0200

    So now -a does not add everything after all?

---
+++ go-rest/src/main/java/com/topdesk/mc12/rest/producers/RuntimeExceptionMapper.java
@@ -0,0 +1,22 @@
+package com.topdesk.mc12.rest.producers;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Provider
+public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
+	@Override
+	public Response toResponse(RuntimeException exception) {
+		log.debug("", exception);
+		return Response.status(Status.INTERNAL_SERVER_ERROR)
+				.entity("Got " + exception.getClass().getSimpleName() + " saying " + exception.getMessage())
+				.type(MediaType.APPLICATION_JSON)
+				.build();
+	}
+}
