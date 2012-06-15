50c1e98dd73441aaf45ccf3b8c647d24cffdeb7a
Krisztian Horvath <K.Horvath@topdesk.com>
2012-06-15 16:32:05 +0200

    Fixed merge

--- go-rules/src/main/java/com/topdesk/mc12/guice/RuleModule.java
+++ go-rules/src/main/java/com/topdesk/mc12/guice/RuleModule.java
@@ -2,10 +2,11 @@ package com.topdesk.mc12.guice;
 
 import com.google.inject.AbstractModule;
 import com.topdesk.mc12.rules.DefaultGoRuleEngine;
+import com.topdesk.mc12.rules.GoRuleEngine;
 
 public class RuleModule extends AbstractModule {
 	@Override
 	protected void configure() {
-		bind(DefaultGoRuleEngine.class).to(DefaultGoRuleEngine.class);
+		bind(GoRuleEngine.class).to(DefaultGoRuleEngine.class);
 	}
 }
