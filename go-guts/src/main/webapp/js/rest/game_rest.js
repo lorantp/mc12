3763d5adf15877a0e9c1e752d6635492165eb808
Jorn Hertsig <jornh@topdesk.com>
2012-06-18 16:55:24 +0200

    Fixed some dependencies and versions

--- go-guts/src/main/webapp/js/rest/game_rest.js
+++ go-guts/src/main/webapp/js/rest/game_rest.js
@@ -1,28 +1,29 @@
 var GAME_REST = function(rest) {
+	var prefix = 'game/';
 	var that = {};
 	
 	that.getGame = function(id, success) {
-		rest.getData('/game/' + id, {}, success);
+		rest.getData(prefix + id, {}, success);
 	};
 	
 	that.doMove = function(gameId, playerId, x, y) {
-		rest.postData('/game/' + gameId + '/move', {playerId: playerId, x: x, y: y});
+		rest.postData(prefix + gameId + '/move', {playerId: playerId, x: x, y: y});
 	};	
 	
 	that.doPass = function(gameId, playerId) {
-		rest.postData('/game/' + gameId + '/pass', {playerId: playerId});
+		rest.postData(prefix + gameId + '/pass', {playerId: playerId});
 	};
 	
 	that.newGame = function(playerId, boardSize, success) {
-		rest.postData('/game/new', {initiatedPlayerId: playerId, boardSize: boardSize}, success);
+		rest.postData(prefix + '/new', {initiatedPlayerId: playerId, boardSize: boardSize}, success);
 	}
 	
 	that.startGame = function(gameId, playerId, boardSize) {
-		rest.postData('/game/' + gameId + '/start', {playerId: playerId, boardSize: boardSize});
+		rest.postData(prefix + gameId + '/start', {playerId: playerId, boardSize: boardSize});
 	}
 	
 	that.cancelGame = function(gameId) {
-		rest.postData('/game/' + gameId + '/cancel', {}, success);
+		rest.postData(prefix + gameId + '/cancel', {}, success);
 	}
 	
 	return that;
