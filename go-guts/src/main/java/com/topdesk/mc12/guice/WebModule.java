d07bfb082ca3ffa8eaab291b7449d3356a3957d2
berndj <b.junglen@topdesk.com>
2012-06-19 17:21:17 +0200

    implementation of authorized player injection

--- go-guts/src/main/java/com/topdesk/mc12/guice/WebModule.java
+++ go-guts/src/main/java/com/topdesk/mc12/guice/WebModule.java
@@ -6,6 +6,9 @@ import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import com.topdesk.mc12.authentication.AuthorizationRequestFilter;
+import com.topdesk.mc12.authentication.PlayerProvider;
+import com.topdesk.mc12.persistence.entities.Player;
 import com.topdesk.mc12.rest.RestInterfaceConfig;
 import com.topdesk.mc12.rest.producers.GoExceptionMapper;
 import com.topdesk.mc12.rest.producers.RuntimeExceptionMapper;
@@ -21,12 +24,14 @@ public class WebModule extends JerseyServletModule {
 		bind(RuntimeExceptionMapper.class).in(Scopes.SINGLETON);
 		bind(GoExceptionMapper.class).in(Scopes.SINGLETON);
 		
+		bind(Player.class).toProvider(PlayerProvider.class);
+		
 		bind(AuthorizationRequestFilter.class).in(Scopes.SINGLETON);
 		bind(GameRestlet.class).to(DefaultGameRestlet.class);
 		
 		serve("/rest/*").with(GuiceContainer.class, ImmutableMap.<String, String> builder()
 				.put(JSONConfiguration.FEATURE_POJO_MAPPING, "true")
-				.put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, "com.topdesk.mc12.guice.AuthorizationRequestFilter")
+				.put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, AuthorizationRequestFilter.class.getName())
 				.build());
 	}
 }
