fc33787197d6c2a3cc96689fba12195f660ca279
Jorn Hertsig <jornh@topdesk.com>
2012-06-20 11:13:58 +0200

    Changed rule engine objects to singletons

--- go-rules/src/main/java/com/topdesk/mc12/guice/RuleModule.java
+++ go-rules/src/main/java/com/topdesk/mc12/guice/RuleModule.java
@@ -1,6 +1,7 @@
 package com.topdesk.mc12.guice;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 import com.topdesk.mc12.rules.DefaultGoRuleEngine;
 import com.topdesk.mc12.rules.GoRuleEngine;
 import com.topdesk.mc12.rules.capturing.CaptureResolver;
@@ -11,8 +12,8 @@ import com.topdesk.mc12.rules.scoring.StoneScoreCalculator;
 public class RuleModule extends AbstractModule {
 	@Override
 	protected void configure() {
-		bind(ScoreCalculator.class).to(StoneScoreCalculator.class);
-		bind(CaptureResolver.class).to(LiveCollectionCaptureResolver.class);
-		bind(GoRuleEngine.class).to(DefaultGoRuleEngine.class);
+		bind(ScoreCalculator.class).to(StoneScoreCalculator.class).in(Scopes.SINGLETON);
+		bind(CaptureResolver.class).to(LiveCollectionCaptureResolver.class).in(Scopes.SINGLETON);
+		bind(GoRuleEngine.class).to(DefaultGoRuleEngine.class).in(Scopes.SINGLETON);
 	}
 }
